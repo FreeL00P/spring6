7f7fc83e69f89b6dec356b32ede7953877a97669
freeloop <freeloop@gmail.com>
2022-11-28 09:56:09 +0800

    log4j2

--- spring-002-frist/src/main/java/com/powernode/spring6/bean/User.java
+++ spring-002-frist/src/main/java/com/powernode/spring6/bean/User.java
@@ -1,7 +1,42 @@
-package com.powernode.spring6.bean;/**
+package com.powernode.spring6.bean;
+
+/**
  * User
- * 
+ *
  * @author fj
  * @date 2022/11/27 21:57
- */public class User {
+ */
+
+public class User {
+    private String name;
+    private int age;
+
+    public String name() {
+        return name;
+    }
+
+    public User setName(String name) {
+        this.name = name;
+        return this;
+    }
+
+    public int age() {
+        return age;
+    }
+
+    public User setAge(int age) {
+        this.age = age;
+        return this;
+    }
+
+    public User() {
+        System.out.println("执行无参构造方法");
+    }
+//    @Override
+//    public String toString() {
+//        return "User{" +
+//                "name='" + name + '\'' +
+//                ", age=" + age +
+//                '}';
+//    }
 }
--- spring-002-frist/src/main/java/com/powernode/spring6/useBean/main.java
+++ spring-002-frist/src/main/java/com/powernode/spring6/useBean/main.java
@@ -1,7 +1,28 @@
-package com.powernode.spring6.useBean;/**
+package com.powernode.spring6.useBean;
+
+import com.powernode.spring6.bean.User;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+
+import java.util.Date;
+
+/**
  * main
- * 
+ *
  * @author fj
  * @date 2022/11/27 22:00
- */public class main {
+ */
+@Slf4j
+public class main {
+    public static void main(String[] args) {
+
+        ClassPathXmlApplicationContext applicationContext = new ClassPathXmlApplicationContext("spring.xml");
+        User user = applicationContext.getBean("user", User.class);
+        User user1 = applicationContext.getBean("user", User.class);
+        System.out.println("user = " + user);
+        System.out.println("user1 = " + user1);
+        Date nowDate = applicationContext.getBean("nowDate", Date.class);
+        System.out.println("nowDate = " + nowDate);
+        System.out.println("new Date() = " + new Date());
+    }
 }
