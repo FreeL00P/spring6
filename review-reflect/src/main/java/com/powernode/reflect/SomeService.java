1afd6575622b10b0ddf06aa16a588bb8b6ba5a29
freeloop <freeloop@gmail.com>
2022-11-29 16:54:18 +0800

    反射练习

---
+++ review-reflect/src/main/java/com/powernode/reflect/SomeService.java
@@ -0,0 +1,7 @@
+package com.powernode.reflect;/**
+ * SomeService
+ * 
+ * @author fj
+ * @date 2022/11/29 15:45
+ */public class SomeService {
+}
---
+++ review-reflect/src/main/java/com/powernode/reflect/invoke_.java
@@ -0,0 +1,7 @@
+package com.powernode.reflect;/**
+ * invoke_
+ * 
+ * @author fj
+ * @date 2022/11/29 15:47
+ */public class invoke_ {
+}
